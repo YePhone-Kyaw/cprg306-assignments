ba2707db7f225618ac46aa131a8b89eafe643b93
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2024-05-13 14:56:27 -0600

    week2 update #2

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <P>Hello World!</P>
+      <p>Hello World!</p>
     </main>
   );
 }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
 
-const githubRepo = 
+const githubRepo = "https://github.com/YePhone-Kyaw/cprg306-assignments.git";
 
 export default function StudentInfo() {
-    <div>Ye Phone Kyaw</div>
-    <Link href={}>CPRG306-Assignments</Link>
+    return(
+        <div>
+        <p>Ye Phone Kyaw</p>
+        <Link href={githubRepo}>CPRG306-Assignments</Link>
+    </div>
+    );   
 }
